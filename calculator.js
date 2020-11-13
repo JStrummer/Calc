c0974678f431a2c0b1a473d660acfb71b27a557d
JStrummer <pescosolidopaolo@gmail.com>
2020-11-13 02:40:16 +0100

    Real initial commit

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
-"use strict"
+'use strict'
 
-var b;
+var display = document.querySelector(".display");
+
+var inputString = "";
+
+var keyboard = document.querySelector(".numbers");
+var operators = document.querySelector(".operators");
+
+var kNumbers = keyboard.querySelectorAll("div");
+var keyOp = operators.querySelectorAll(":not(#CE)");
+
+kNumbers.forEach( (el) => el.addEventListener("click",
+getInput));
+
+function getInput (evt) {
+  inputString += evt.target.innerText;
+  display.innerText = inputString;
+}

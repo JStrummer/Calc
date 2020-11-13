27a1a2bf9229f8c6b2b7d7e4dfa7ccc78db54d4d
JStrummer <pescosolidopaolo@gmail.com>
2020-11-13 12:42:01 +0100

    Some changes

--- calculator.js
+++ calculator.js
@@ -1,19 +1,36 @@
 'use strict'
 
+// get HTML element
 var display = document.querySelector(".display");
 
-var inputString = "";
+var keyNumbers = document.querySelectorAll(".num");
 
-var keyboard = document.querySelector(".numbers");
-var operators = document.querySelector(".operators");
+var keyOps = document.querySelectorAll(".op");
 
-var kNumbers = keyboard.querySelectorAll("div");
-var keyOp = operators.querySelectorAll(":not(#CE)");
+var keyTotal = document.getElementById("equal");
 
-kNumbers.forEach( (el) => el.addEventListener("click",
-getInput));
+var keyCE = document.getElementById("CE");
+
+var keyComma = document.getElementById("comma");
 
 function getInput (evt) {
-  inputString += evt.target.innerText;
-  display.innerText = inputString;
+  inputs.push(evt.target.innerText);
+  display.innerText = displayInput();
+}
+
+// handling inouts
+var inputs = ["01"];
+
+function reset (array) {
+  array = ["0"];
+}
+
+function lastChar(array) {
+  var lastInput = array[array.length - 1];
+  return lastInput[lastInput.length - 1];
+}
+
+function deleteLastChar (array) {
+  var lastInput = array[array.length - 1];
+  lastInput = lastInput.slice(0, lastInput.length - 1);
 }
